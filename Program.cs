81a0287861b846856b293285aca26e5f143b5a10
1Chip1 <antirealsss@gmail.com>
2023-03-29 13:32:08 +0300

    Added Menu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace HotelManager {
         List<int> usedRooms = new List<int>();
 
         void menu() {
+            Console.WriteLine("Welcome To Hotel Manager!");
+            Console.WriteLine("What Would YoU Like To Do?");
+            Console.WriteLine("1. Inform Of A Used Room");
+            Console.WriteLine("2. Infrom Of A Free Room");
+            Console.WriteLine("3. See What Rooms Are Free And What Are Used");
+            Console.WriteLine("4. Exit");
 
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            if(choice == 1) {
+                                  
+            } else if(choice == 2) {
+                free_A_Room();
+            } else if (choice == 3) {
+                Console.WriteLine("Free Rooms: " + freeRooms);
+                Console.WriteLine("Used Room: " + usedRooms);
+                menu();
+            } else if(choice == 4) {
+                return;
+            }
         }
 
         void free_A_Room() {
