151fa2fd349be7d363a50814f34f8ef8ca5f8ef3
1Chip1 <antirealsss@gmail.com>
2023-03-29 12:47:06 +0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace HotelMenagmets {
+    class Program {
+        List<int> freeRooms = new List<int> {1, 2, 3, 4, 5};
+        static void Main(string[] args) {
+            
+        }
+    }
+}
