b288813c6a419d206047c10415276eaaf5cdec89
1Chip1 <antirealsss@gmail.com>
2023-03-29 13:20:12 +0300

    Added a free room function and menu

--- Program.cs
+++ Program.cs
@@ -1,8 +1,29 @@
 ﻿using System;
 
-namespace HotelMenagmets {
+namespace HotelManager {
     class Program {
         List<int> freeRooms = new List<int> {1, 2, 3, 4, 5};
+        List<int> usedRooms = new List<int>();
+
+        void menu() {
+
+        }
+
+        void free_A_Room() {
+            Console.WriteLine("Which Room Do You Want To Free?");
+            int room = Convert.ToInt32(Console.ReadLine());
+
+            foreach (int rooms in usedRooms) {
+                if (rooms == room) {
+                    usedRooms.Remove(rooms);
+                    freeRooms.Add(rooms);
+                    Console.WriteLine("Rooms Is Now Free");
+                    menu();
+                } 
+            }
+
+        } 
+
         static void Main(string[] args) {
             
         }
