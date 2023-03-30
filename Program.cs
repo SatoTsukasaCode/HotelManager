45a064a1056794c5ccaf9b99f68b6e777298d824
SatoTsukasa <78177853+SatoTsukasaCode@users.noreply.github.com>
2023-03-30 15:25:54 +0300

    accidently destroyed  the code

--- Program.cs
+++ Program.cs
@@ -1,9 +1,24 @@
-﻿using System;
+using System;
 
 namespace HotelManager {
     class Program {
         List<int> freeRooms = new List<int> {1, 2, 3, 4, 5};
         List<int> usedRooms = new List<int>();
+
+        void freeRoom() {
+            Console.WriteLine("Which Room Do You Want To Free?");
+            int room = Convert.ToInt32(Console.ReadLine());
+
+            foreach (int rooms in usedRooms) {
+                if (rooms == room) {
+                    usedRooms.Remove(rooms);
+                    freeRooms.Add(rooms);
+                    Console.WriteLine("Rooms Is Now Free");
+                    menu();
+                } 
+            }
+
+        }
 
         void useRooms() {
             Console.WriteLine("Which Room Do You Want To Inform That is used?");
